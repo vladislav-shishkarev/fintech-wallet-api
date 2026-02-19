4736894316794eb644cc317e5ac07ccdf2823231
Vladislav Shishkarev <vladislavshishkarev@gmail.com>
2026-02-19 16:27:24 +0300

    Добавлен эндпоинт "Ping"

--- app/main.py
+++ app/main.py
@@ -3,4 +3,10 @@ from fastapi import FastAPI
 app = FastAPI()
 
 
-
+@app.get("/ping")
+async def server_check():
+    """
+    Health check endpoint.
+    Returns the status of the server to ensure it's running correctly.
+    """
+    return {"status": "successful"}
