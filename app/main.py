c574c7140ee9dec57adf1c6f6d1b35d3420258c9
Vladislav Shishkarev <vladislavshishkarev@gmail.com>
2026-02-25 19:24:47 +0300

    Configure divided FastAPI structure

---
+++ app/main.py
@@ -0,0 +1,13 @@
+from fastapi import FastAPI
+
+
+app = FastAPI()
+
+
+@app.get("/ping")
+async def server_check():
+    """
+    Health check endpoint.
+    Returns the status of the server to ensure it's running correctly.
+    """
+    return {"status": "successful"}
