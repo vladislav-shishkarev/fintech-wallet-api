563d0df45d01bcdbfd56b3fc104e6b6a85754776
Vladislav Shishkarev <vladislavshishkarev@gmail.com>
2026-03-01 16:03:27 +0300

    feat (db): Safe database link build and usage

--- migrations/env.py
+++ migrations/env.py
@@ -6,14 +6,15 @@ from sqlalchemy.engine import Connection
 from sqlalchemy.ext.asyncio import async_engine_from_config
 
 from alembic import context
-from app.db import Base, DATABASE_URL
+from app.db import Base
+from app.config import settings
 import app.models
 
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
 config = context.config
-config.set_main_option("sqlalchemy.url", DATABASE_URL)
+config.set_main_option("sqlalchemy.url", settings.db_url)
 
 # Interpret the config file for Python logging.
 # This line sets up loggers basically.
