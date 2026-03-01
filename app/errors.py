81136b84206d631313dcdfd593609be6f3e8fd96
Vladislav Shishkarev <vladislavshishkarev@gmail.com>
2026-03-01 12:58:48 +0300

    feat (errors): Add error class for missing wallet

--- app/errors.py
+++ app/errors.py
@@ -2,3 +2,9 @@ class UserNotFoundError(Exception):
     def __init__(self, id: int):
         self.id = id
         super().__init__(f'User not found: ID {self.id}')
+
+
+class WalletNotFoundError(Exception):
+    def __init__(self, id: int):
+        self.id = id
+        super().__init__(f'Wallet not found: ID {self.id}')
